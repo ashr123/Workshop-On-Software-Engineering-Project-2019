149a684e4231132f19ba933bbb094f0027088dd6
Rotem <rotba@post.bgu.ac.il>
2019-03-24 21:48:19 +0200

    Traceability table, Service Facade sketch

--- dev/AcceptanceTests/Test.py
+++ dev/AcceptanceTests/Test.py
@@ -1,5 +1,8 @@
 from main.Service.Facade import Facade
 
+def test_setup():
+    assert False
+
 def test_login():
     facade_instance = Facade()
     assert True == facade_instance.login("rotem", "123456")
