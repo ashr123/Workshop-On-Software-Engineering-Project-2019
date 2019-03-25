63b4a0e9c8323e0b60fbf2deb5b128e6c593d44c
noaleshem <noaleshem03@gmail.com>
2019-03-26 01:07:25 +0200

    nop

---
+++ dev/build/lib/main/Domain/Discount.py
@@ -0,0 +1,7 @@
+from main.Domain.Rule import Rule
+
+
+class Discount(Rule):
+
+	def __init__(self):
+		Rule.__init__(self)
---
+++ dev/build/lib/main/Domain/Purchase.py
@@ -0,0 +1,7 @@
+from main.Domain.Rule import Rule
+
+
+class Purchase(Rule):
+
+	def __init__(self):
+		Rule.__init__(self)
