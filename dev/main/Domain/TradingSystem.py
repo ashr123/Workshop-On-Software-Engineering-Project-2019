1a0bbe1e6ad0e48e55a70ec359d04df83048370b
Roy Ash <roykid123@gmail.com>
2019-03-27 18:25:43 +0200

    שינויים זמניים
    Signed-off-by: Roy Ash <roykid123@gmail.com>

--- dev/main/Domain/TradingSystem.py
+++ dev/main/Domain/TradingSystem.py
@@ -1,32 +1,40 @@
 from .TradingSystemException import UserAlreadyExistException
 from .Guset import Guest
 from .Member import Member
+from .Store import Store
+from .User import User
+from typing import *
 
 
 class TradingSystem(object):
 
 	def __init__(self):
-		self._users = {}
-		self._members = []
-		self._stores = []
+		self._users: Dict[int, User] = {}
+		self._members: List[Member] = []
+		self._stores: List[Store] = []
 
-	def get_user(self, session_id):
-		return "h"
+	def get_user(self, session_id: int) -> User:
+		return self._users[session_id]
 
-	def get_member(self, member_name):
+	def get_member(self, member_name: str) -> Optional[Member]:
 		if member_name in map(lambda member: member.name, self._members):
-			return filter(lambda member: member.name == member_name, self._members)[0]
+			return list(filter(lambda member: member.name == member_name, self._members))[0]
 		return None
 
-	def genarate_id(self):
-		newSessionId = len(self._users)
-		self._users[newSessionId] = Guest(tradingSystem=self)
-		return newSessionId
+	def generate_id(self) -> int:
+		"""
+		Generates new session ID
+		:rtype: int
+		:return: The new ID
+		"""
+		new_session_id: int = len(self._users)
+		self._users[new_session_id] = Guest(tradingSystem=self)
+		return new_session_id
 
 	def search(self, keyword):
 		return False
 
-	def registerMember(self, sessionId, username, password):
+	def register_member(self, session_id: int, username: str, password: str) -> bool:
 		if username in map(lambda m: m.name, self._members):
 			raise UserAlreadyExistException(message="the user {} is already registered".format(username))
-		self._members[sessionId] = Member(username)  # TODO - handle security
+		self._members[session_id] = Member(username)  # TODO - handle security
--- dev/main/Domain/TradingSystemException.py
+++ dev/main/Domain/TradingSystemException.py
@@ -1,22 +1,28 @@
 class TradingSystemException(Exception):
-	def __init__(self, message, errors):
-		super().__init__(message)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
+
 
 class UserAlreadyExistException(TradingSystemException):
-	def __init__(self, message, errors):
-		super().__init__(message,errors)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
+
 
 class UserAlreadyHasStoreException(TradingSystemException):
-	def __init__(self, message, errors):
-		super().__init__(message,errors)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
+
 
 class GusetCannotOpenStoreException(TradingSystemException):
-	def __init__(self, message, errors):
-		super().__init__(message,errors)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
+
 
 class AnomalyException(TradingSystemException):
-	def __init__(self, message, errors):
-		super().__init__(message,errors)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
+
+
 class PermissionException(TradingSystemException):
-	def __init__(self, message, errors):
-		super().__init__(message,errors)
+	def __init__(self, message, errors=None):
+		super().__init__(message, errors)
