56695f6a7383614e64ecd60009c8dfd3c5af4abb
Rotem <rotba@post.bgu.ac.il>
2019-03-29 17:37:05 +0300

    unit tests ready for main.domain.Store

--- dev/main/domain/Store.py
+++ dev/main/domain/Store.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 
 from .Item import Item
@@ -22,18 +23,38 @@ class Store(object):
 	def add_item(self, new_item: Item):
 		self._items.append(new_item)
 
-	def remove_item(self, item_name: str) -> bool:  # TODO fix
+	def remove_item(self, item_id: int) -> bool:  # TODO fix
 		# if not item_name in map(lambda m: m.id, self._items):
 		# 	raise PermissionException(message="this item id {} is not in store stock !".format(item_name))
 		for item in self._items:
-			if item.id == item_name:
+			if item.id == item_id:
 				self._items.remove(item)
 				return True
 		return False
 
-	def edit_item(self, item_name: str, new_price: float = None, new_name: str = None):  # TODO implement
-		return False
+	def edit_item(self, item_id: int, new_price: float = None, new_name: str = None):  # TODO implement
+		item = self.get_item(item_id)
+		if item ==None:
+			return False
+		if not new_name == None:
+			item.name =new_name
+		if not new_price == None:
+			item.price =new_price
+		self.remove_item(item_id=item_id)
+		self.add_item(new_item=item)
+		return True
+
+
 
 	def add_owner(self, owner: Member):
 		self._managers.append(owner)
+
+	def has_item(self, item_id: int) -> bool:
+		return item_id in list(map(lambda i: i.id,self._items))
+
+	def get_item(self, item_id: int) -> Item:
+		if not self.has_item(item_id):
+			return None
+		item = list(filter(lambda i: i.id == item_id,self._items))[0]
+		return copy.deepcopy(item)
 
