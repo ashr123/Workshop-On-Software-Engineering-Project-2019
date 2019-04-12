77403d8090b84856b8beb8aace6594ab88781f2f
Roy Ash <roykid123@gmail.com>
2019-04-12 12:59:04 +0300

    trying to make it work

--- dev/TradingSystemFrontEnd/views.py
+++ dev/TradingSystemFrontEnd/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    # return HttpResponse('Hello from Django!')
+    list_teams = Team.objects.filter(team_level__exact="U09")
+    context = {'youngest_teams': list_teams}
+    return render(request, 'home.html', context)
