738af4404c3db9baaeed045c7136a8742d857e9b
Roy Ash <roykid123@gmail.com>
2019-04-22 20:08:00 +0300

    דגכדג

--- dev/dev/urls.py
+++ dev/dev/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
 	path('home/', include('home.urls')),
 	path('admin/', admin.site.urls),
+	path('', RedirectView.as_view(url='/home/', permanent=True))
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- dev/home/urls.py
+++ dev/home/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('initiate', views.initiate),
-	path('', RedirectView.as_view(url='/home/', permanent=True))
 ]
