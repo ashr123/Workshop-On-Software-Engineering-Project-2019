a87bd2c101f20bd96239ff113abd6992437484b5
Roy Ash <roykid123@gmail.com>
2019-04-27 20:46:50 +0300

    starting to create store managment to owner

--- dev/trading_system/views.py
+++ dev/trading_system/views.py
@@ -6,4 +6,16 @@ def index(request):
     return render(request, 'trading_system/index.html')
 
 def register(request):
-    return render(request, 'trading_system/register.html')
+	return render(request, 'trading_system/register.html')
+
+
+def search(request):
+	text = SearchForm(request.GET)
+	if text.is_valid():
+		context = {'title': 'items', 'results': {'3': 4, '2': 6}}
+
+	return render(request, 'search_results.html', context)
+
+
+def item(request, id):
+	return render(request, 'item_de.html')
