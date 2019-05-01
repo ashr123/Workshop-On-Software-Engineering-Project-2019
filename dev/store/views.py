addece4b56947a8665b061f0c1575ee9268cb626
elhananperel <elhananp@post.bgu.ac.il>
2019-05-01 22:53:50 +0300

    redirect homepage storeower

--- dev/store/views.py
+++ dev/store/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.conf import settings
 from django.views.generic import DetailView
 
 from .models import Store, Item
 from . import forms
-from django.contrib.auth.models import Group ,User
+from django.contrib.auth.models import Group, User
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
+
 
 # Create your views here.
 def add_store(request):
@@ -20,26 +21,21 @@ def submit_open_store(request):
 	if open_store_form.is_valid():
 		store = Store.objects.create(name=open_store_form.cleaned_data.get('name'),
 		                             owner_id=int(request.session._session['_auth_user_id']))
-		# item1 = Item(name='dd')
-		# item1.save()
-		# store.items.add(item1)
 		store.save()
-
-	stores = Store.objects.filter(owner_id=int(request.session._session['_auth_user_id']))
+	# in error masssege!!!!!!!!!!!!!
+	# stores = Store.objects.filter(owner_id=int(request.session._session['_auth_user_id']))
 	my_group = Group.objects.get(name="store_owners")
 	# user = User.objects.get(username=request.user.username)
 	request.user.groups.add(my_group)
-
-	#stores = Store.objects.get(owner_id=int(request.session._session['_auth_user_id']))[0]
-
-	context = {'title': 'stores:', 'results': stores}
-
-	return render(request, 'store/homepage_store_owner.html', context)
-
+	# stores = Store.objects.get(owner_id=int(request.session._session['_auth_user_id']))[0]
+	#context = {'title': 'stores:', 'results': stores}
+	#return render(request, 'store/homepage_store_owner.html', context)
+	return redirect('/store/home_page_owner/')
 
 class StoreDetailView(DetailView):
 	model = Store
 	paginate_by = 100  # if pagination is desired
+
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		return context
