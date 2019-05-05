6839e7ef23b5a25c72eec9dc387d02823c8b5363
Rotem <rotba@post.bgu.ac.il>
2019-05-05 15:03:22 +0300

    cart stuf

--- dev/trading_system/models.py
+++ dev/trading_system/models.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.models import User
+from store.models import Store, Item
 from django.db import models
 
 # Create your models here.
 # Create your models here.
+
+class Cart(models.Model):
+	customer = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+	store = models.ForeignKey(Store, on_delete=models.CASCADE, default=None)
+	items = models.ManyToManyField(Item)
+
+	class Meta:
+		unique_together = (("customer", "store"),)
--- dev/trading_system/urls.py
+++ dev/trading_system/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
 	path('home_button/', views.home_button),
 	path('login_redirect', views.login_redirect),
 	path('cart/', views.show_cart),
-	path('view_items/', views.SearchListView.as_view(), name='item-list',)
+	path('view_items/', views.SearchListView.as_view(), name='item-list', ),
+	path('add_item_to_cart/<int:item_pk>', views.add_item_to_cart),
+	path('view_carts', views.CartsListView.as_view()),
+	path('view_cart/<int:pk>', views.CartDetail.as_view()),
+
 ]
