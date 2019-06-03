2c13849954b377016925f4f4a055812e5f7005ab
elhananperel <elhananp@post.bgu.ac.il>
2019-06-03 22:12:09 +0300

    add super user at start

--- dev/trading_system/urls.py
+++ dev/trading_system/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index),
+	path('super_user', views.def_super_user),
 	# path(r'^item_page/(?P<id>\d+)/$', views.item),
 	path('item_page/<int:id>/', views.item),
 	path('home_button/', views.home_button),
@@ -21,6 +22,5 @@ urlpatterns = [
 	path('join_auction/<int:item_pk>', views.join_auction),
 	path('view_auctions', views.AuctionsListView.as_view()),
 	path('view_auction/<int:auction_pk>', views.view_auction),
-
 
 ]
